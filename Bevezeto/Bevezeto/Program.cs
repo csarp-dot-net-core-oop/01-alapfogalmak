39c028da0fc998fc6f17e6406cea1db866d83499
Csaba Gyuris <gyuris.csaba@vasvari.org>
2021-01-09 17:19:13 +0100

    O1.01 Cat(macska) osztály

--- Bevezeto/Bevezeto/Program.cs
+++ Bevezeto/Bevezeto/Program.cs
@@ -2,10 +2,21 @@
 
 namespace Bevezeto
 {
+    /// <summary>
+    /// O1.01 Cat(macska) osztály
+    /// Az osztály egy kiterjesztett adat típus. Az osztály egy program kód sablon, amelyben 
+    /// állapotokat (adatokat) és viselkedési mintákat, műveleteket (metódusokat) lehet definiálni.
+    /// Az osztályok neveit nagybetűvel (PascalCase) írjuk.
+    /// Használjunk főneveket az osztály nevének megadásakor.
+    /// </summary>
+    class Cat
+    {
+    }
+
     class Program
     {
         static void Main(string[] args)
-        {            
-        }
+        {
+        }            
     }
 }
