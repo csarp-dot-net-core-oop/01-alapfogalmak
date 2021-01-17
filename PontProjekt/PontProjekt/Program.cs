4cc2eadba4b7452302a99dd135ad3f522af0f177
Csaba Gyuris <gyuris.csaba@vasvari.org>
2021-01-17 16:37:33 +0100

    P1.02 A pont osztály adattagjai (mezők)

--- PontProjekt/PontProjekt/Program.cs
+++ PontProjekt/PontProjekt/Program.cs
@@ -7,6 +7,15 @@ namespace PontProjekt
     /// </summary>
     class Point
     {
+        /// <summary>
+        /// P1.02 A pont osztály adattagjai (mezők)
+        /// A tárolt adatok az objektum pillanatnyi állapotát írják le.
+        /// Az adattagokat private elérhetősségel adjuk meg egyenlőre.
+        /// Az osztály adattag változóit az osztály elején deklaráljuk.
+        /// Ne használjunk aláhúzást a változók nevében.
+        /// Az adattagok esetén cameCase névkonvenciót használunk.
+        /// Az adattagok nevei leíróak legyenek.
+        /// </summary>
     }
     class Program
     {
