7f1951860c08660ccaebe65b8c03f45909f2c6f7
Csaba Gyuris <gyuris.csaba@vasvari.org>
2021-01-22 15:20:30 +0100

    Point osztály adattagokkal és konstruktorral

--- SzakaszProjekt/SzakaszProjekt/Program.cs
+++ SzakaszProjekt/SzakaszProjekt/Program.cs
@@ -2,6 +2,40 @@
 
 namespace SzakaszProjekt
 {
+    /// <summary>
+    /// Pont osztály.
+    /// Geometriai pont két valós koordinátával és egy karakteres névvel.
+    /// </summary>
+    class Point
+    {
+        /// <summary>
+        /// A pont egy karakteres elnevezése.
+        /// </summary>
+        private char name;
+        /// <summary>
+        /// A pont x koordinátája.
+        /// </summary>
+        private double x;
+        /// <summary>
+        /// A pont x koordinátája.
+        /// </summary>
+        private double y;
+
+        /// <summary>
+        /// Az osztály minden tulajdonságát megadó konstruktora.
+        /// A konstruktornak nincs visszatérési értéke.
+        /// </summary>
+        /// <param name="name">A pont elnevezése</param>
+        /// <param name="x">A pont első koordinátája</param>
+        /// <param name="y">A pont második koordinátája</param>
+        public Point(char name, double x, double y)
+        {
+            this.name = name;
+            this.x = x;
+            this.y = y;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
